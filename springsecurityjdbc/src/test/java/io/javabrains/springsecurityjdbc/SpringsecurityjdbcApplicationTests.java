230a3ec3bb4e25312c475604f956d2afd143b2ca
RESTY NASIMBWA <54865360+restynan@users.noreply.github.com>
2020-04-06 16:30:17 -0500

    springsecurity with JDBC

---
+++ springsecurityjdbc/src/test/java/io/javabrains/springsecurityjdbc/SpringsecurityjdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package io.javabrains.springsecurityjdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityjdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
