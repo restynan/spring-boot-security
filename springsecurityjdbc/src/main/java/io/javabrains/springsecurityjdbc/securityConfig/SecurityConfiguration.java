9fe87afbc8f9481415632fd18f1d8d084416d72c
RESTY NASIMBWA <54865360+restynan@users.noreply.github.com>
2020-04-11 15:01:10 -0500

    usersByUsernameQuery

--- springsecurityjdbc/src/main/java/io/javabrains/springsecurityjdbc/securityConfig/SecurityConfiguration.java
+++ springsecurityjdbc/src/main/java/io/javabrains/springsecurityjdbc/securityConfig/SecurityConfiguration.java
@@ -18,7 +18,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-        auth.jdbcAuthentication().dataSource( dataSource);
+        auth.jdbcAuthentication().dataSource( dataSource)
+        .usersByUsernameQuery("select username,password,enabled"
+                + "from users"
+                +"where username = ?")
+                .authoritiesByUsernameQuery("select username ,authority"
+                +"from  authorities"
+                        +"where username = ?");
+
 
 
     }
