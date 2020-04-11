0fee17b4a15c57d93672e7e28b6cbe764eb954da
RESTY NASIMBWA <54865360+restynan@users.noreply.github.com>
2020-04-11 13:55:33 -0500

    h2 connection

---
+++ springsecurityjdbc/src/main/java/io/javabrains/springsecurityjdbc/controller/homeController.java
@@ -0,0 +1,20 @@
+package io.javabrains.springsecurityjdbc.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class homeController {
+    @RequestMapping("/home")
+    public String  home(){
+        return "Welcome";
+    }
+    @RequestMapping("/user")
+    public String  user(){
+        return "welcome User";
+    }
+    @RequestMapping("/admin")
+    public String  admin(){
+        return "Welcome admin";
+    }
+}
